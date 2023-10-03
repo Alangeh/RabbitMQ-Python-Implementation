9844dc3f3bbcec6fa7af050475f154c5561e4493
Lionel Ngobesing <ngobesinglionel@gmail.com>
2023-10-03 14:07:05 +0300

    implementing shared routing keys

--- Routing/analyticsconsumer.py
+++ Routing/analyticsconsumer.py
@@ -15,6 +15,7 @@ channel.exchange_declare(exchange='routing', exchange_type=ExchangeType.direct)
 queue = channel.queue_declare(queue='', exclusive=True)
 
 channel.queue_bind(exchange='routing', queue=queue.method.queue, routing_key='analyticsonly')
+channel.queue_bind(exchange='routing', queue=queue.method.queue, routing_key='both')
 
 channel.basic_consume(queue=queue.method.queue, auto_ack=True, on_message_callback=on_message_received)
 
--- Routing/producer.py
+++ Routing/producer.py
@@ -11,7 +11,8 @@ channel.exchange_declare(exchange='routing', exchange_type=ExchangeType.fanout)
 
 message = "Hello, routing implementation. Routed Message"
 
-channel.basic_publish(exchange='pubsub', routing_key='analyticsonly', body=message)
+channel.basic_publish(exchange='routing', routing_key='analyticsonly', body=message)
+channel.basic_publish(exchange='routing', routing_key='both', body=message)
 
 print(f"Message sent: {message}")
 
