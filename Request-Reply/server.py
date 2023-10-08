493ee5d9ce57415971d54495112f18f241aa53cf
Lionel Ngobesing <ngobesinglionel@gmail.com>
2023-10-08 23:17:21 +0300

    setting up the server for request reply

--- Request-Reply/server.py
+++ Request-Reply/server.py
@@ -1,7 +1,8 @@
 import pika
 
-def on_message_received(ch, method, properties, body):
-    print(f"received new message: {body}")
+def on_request_message_received(ch, method, properties, body):
+    print(f"Request received: {properties.correlation_id}")
+    ch.basic_publish('', routing_key=properties.reply_to, body=f'Hey its your reply to {properties.correlation_id}')
 
 connection_parameters = pika.ConnectionParameters('localhost')
 
@@ -9,9 +10,9 @@ connection = pika.BlockingConnection(connection_parameters)
 
 channel = connection.channel()
 
-channel.queue_declare(queue='letterbox')
+channel.queue_declare(queue='request-queue')
 
-channel.basic_consume(queue='letterbox', auto_ack=True, on_message_callback=on_message_received)
+channel.basic_consume(queue='request-queue', auto_ack=True, on_message_callback=on_request_message_received)
 
 print("start consuming")
 
