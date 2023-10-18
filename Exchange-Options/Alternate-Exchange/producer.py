a070fcead5a891511612e1750860bee70ea6994c
Lionel Ngobesing <ngobesinglionel@gmail.com>
2023-10-18 20:53:37 +0300

    Alternate Exchanges, creating the producer

---
+++ Exchange-Options/Alternate-Exchange/producer.py
@@ -0,0 +1,24 @@
+import pika
+from pika.exchange_type import ExchangeType
+
+connection_parameters = pika.ConnectionParameters("localhost")
+
+connection = pika.BlockingConnection(connection_parameters)
+
+channel = connection.channel()
+
+channel.exchange_declare(queue='altExchange', exchange_type=ExchangeType.fanout)
+
+channel.exchange_declare(
+    queue='mainexchange',
+    exchange_type=ExchangeType.direct,
+    arguments={'alternate-exchange': 'altExchange'}
+)
+
+message = 'hellow thsi is alternaate exchange example'
+
+channel.basic_publish(exchange='mainexchange', routing_key='test', body=message)
+
+print(f'sent message: {message}')
+
+connection.close()
