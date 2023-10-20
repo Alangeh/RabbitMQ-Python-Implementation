938229632859fd6af9bd810626428191731d9544
Lionel Ngobesing <ngobesinglionel@gmail.com>
2023-10-20 13:47:42 +0300

    consumer

--- Exchange-Options/Accept-Reject-Messages/consumer.py
+++ Exchange-Options/Accept-Reject-Messages/consumer.py
@@ -3,7 +3,8 @@ from pika.exchange_type import ExchangeType
 
 def on_message_received(ch, method, properties, body):
 
-    ch.basic_ask(delivery_tag=method.delivery_tag, multiple=False)
+    if(method.deliver_tage % 5 == 0):
+        ch.basic_ask(delivery_tag=method.delivery_tag, multiple=False)
 
     print(f'received message: {body}')
 
